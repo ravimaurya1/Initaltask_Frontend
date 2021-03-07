a566db162c4fdfca513c7aa129351ec5d35afeeb
RAVI MAURYA <ravimaurya@QMAC0238.local>
2021-03-07 10:28:19 +0530

    Some files are deleted

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 import { ApolloProvider } from "@apollo/client";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 // ApolloClient
 const client = new ApolloClient({
@@ -20,3 +21,9 @@ ReactDOM.render(
   </ApolloProvider>,
   document.getElementById("root")
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
+
